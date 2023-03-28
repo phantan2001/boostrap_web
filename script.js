017ca31147e992159a4c5efea264a310a193ba54
Phan Tan <pttan1304@gmail.com>
2023-03-28 17:56:52 +0700

    ok done

---
+++ script.js
@@ -0,0 +1,10 @@
+const redirect = document.getElementById("about");
+redirect.addEventListener("click", function() {
+    window.location.replace("about_us.html");
+});
+
+
+const redirect_2 = document.getElementById("about-2");
+redirect_2.addEventListener("click", function() {
+    window.location.replace("about_us.html");
+})
